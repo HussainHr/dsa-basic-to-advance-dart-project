0131cd7f4becec11cdb1f99d6ea6dfb6b972d11e
unknown <mhr163769@gmail.com>
2024-09-20 16:37:22 +0600

    Initialize first dart dsa project commit

---
+++ dsa_dart_application/bin/dsa_dart_application.dart
@@ -0,0 +1,5 @@
+import 'package:dsa_dart_application/dsa_dart_application.dart' as dsa_dart_application;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dsa_dart_application.calculate()}!');
+}
---
+++ dsa_dart_application/test/dsa_dart_application_test.dart
@@ -0,0 +1,8 @@
+import 'package:dsa_dart_application/dsa_dart_application.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
